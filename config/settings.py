e414d521e1604d66aa63f8b99b9aafb59c0dc17d
Massy-dev <massy1833.dev@gmail.com>
2026-03-24 23:23:19 +0000

    Resolution bug Premier deploiement

--- config/settings.py
+++ config/settings.py
@@ -83,18 +83,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True)
 }
-"""DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ajul_db',
-        'USER': 'posgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'db',
-        'PORT': '5432',
-    }
-}"""
 
 
 # Password validation
